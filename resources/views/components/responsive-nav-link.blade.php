7ae0b62af199198b2d8e0ccc2cdbcd4f73c17afc
Kazhtova <kasshhhoktora@gmail.com>
2026-07-05 08:27:40 +0700

    thirtthree -2 commit

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-slate-800 text-start text-base font-medium text-slate-700 bg-slate-100 focus:outline-none focus:text-slate-800 focus:bg-slate-100 focus:border-slate-700 transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-slate-100 hover:border-slate-700 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/layouts/navigations.blade.php
+++ resources/views/layouts/navigations.blade.php
@@ -64,11 +64,11 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
+                            <x-responsive-nav-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
-                            </x-dropdown-link>
+                            </x-responsive-nav-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
