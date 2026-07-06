69d2d640bc1dbefe19a979f4d3101d6585cdf0b9
Kazhtova <kasshhhoktora@gmail.com>
2026-07-06 09:53:18 +0700

    thirtyfour -4 commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" href="{{ asset('images/M.svg') }}" type="image/svg+xml">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
